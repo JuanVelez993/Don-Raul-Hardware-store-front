0e5caa4f98b94dbc5f9944b0c0240b35a6e1749d
Juan Pablo Velez <juan.velez993@gmail.com>
2022-06-08 13:13:02 -0500

    ADD--firebase config

---
+++ src/firebase-config.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlY2j3TIJiFaEdmmHA5knAeN817AQF_l4",
+    authDomain: "don-raul-hardware-store.firebaseapp.com",
+    projectId: "don-raul-hardware-store",
+    storageBucket: "don-raul-hardware-store.appspot.com",
+    messagingSenderId: "42584980549",
+    appId: "1:42584980549:web:42b5df535d9eefd8d7b72c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/main/java/com/sofkau/DonRaulHardwarestore/DonRaulHardwareStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.DonRaulHardwarestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonRaulHardwareStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonRaulHardwareStoreApplication.class, args);
+	}
+
+}
